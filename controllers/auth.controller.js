b90f1b1cae62b2e2eb1657b68f73a88fb9d96ccd
Qitmeershehzadghzi <qitmeershehzad@gmail.com>
2026-05-12 23:31:00 +0500

    add prisma and auth contoller functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,33 @@
 import bcrypt from "bcrypt";
-export const register = (req, res) => {
-const { username, email, password } = req.body;
+import {prisma} from "../lib/prisma.js";
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
 
-// password hashing by bcrypt
-const hashPassword =bcrypt.hash(password,10)
+  try {
+    // password hashing by bcrypt
+    const hashPassword = await bcrypt.hash(password, 10);
 
+    // create new user in database
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashPassword,
+      },
+    });
 
+    res.status(201).json({
+      message: "User registered successfully",
+      user: newUser,
+    });
 
-
-
-
-}
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+};
 export const login = (req, res) => {
 
 }
